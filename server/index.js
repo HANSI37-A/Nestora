84a21ec46af87389492b0d71eab4440211cdf88b
HANSI37-A <tharaki115h@gmail.com>
2026-05-20 22:20:38 +0530

    Initial backend setup: configured gitignore and added core MERN structure

--- server/index.js
+++ server/index.js
@@ -1,22 +1,27 @@
 const express = require('express');
-const dotenv = require('dotenv');
 const cors = require('cors');
-const connectDB = require('./db');
+const dotenv = require('dotenv');
 
 dotenv.config();
 
-connectDB();
+const connectDB = require('./config/db');
+const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+connectDB();
+
 app.get('/', (req, res) => {
-  res.json({ message: ' Nestora API is running...' });
+  res.json({ message: 'Nestora API is running...' });
 });
 
+app.use('/api/users', userRoutes);
+
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
-  console.log(` Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
