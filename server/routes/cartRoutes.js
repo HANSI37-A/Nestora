00af90e373a54be36f7122df3ca1de41fd1d1e42
HANSI37-A <tharaki115h@gmail.com>
2026-05-24 02:19:07 +0530

    Fix cart retrieval logic and enhance delete functionality; ensure proper quantity handling and return appropriate responses

--- server/routes/cartRoutes.js
+++ server/routes/cartRoutes.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 const getCart = async (userId, guestId) =>{
   if (userId) {
-    return await Cart.findOne({ userId });
+    return await Cart.findOne({ user: userId });
   } else if (guestId) {
     return await Cart.findOne ({ guestId });
   }
@@ -100,8 +100,8 @@ router.put("/", async (req, res) =>{
 
     if (productIndex > -1) {
       //update quentity
-      if(quantity > 0){
-        cart.products[productIndex].quantity = quantity;
+      if(Number(quantity) > 0){
+        cart.products[productIndex].quantity = Number(quantity);
       } else {
         cart.products.splice(productIndex, 1);
       }
@@ -119,4 +119,37 @@ router.put("/", async (req, res) =>{
   }
 });
 
+// @route DELETE /api/cart
+// @desc Remove a product from the cart
+// @access Public
+router.delete("/", async(req, res) =>{
+  const { productId, size, color, guestId, userId} = req.body;
+  try{ 
+    let cart = await getCart(userId, guestId);
+    if (!cart) return res.status(404).json({ message: "Cart not found"});
+    const productIndex = cart.products.findIndex(
+      (p)=>
+        p.productId.toString() === productId && p.size === size && p.color === color
+    );
+
+    if (productIndex > -1){
+      cart.products.splice(productIndex, 1);
+
+      cart.totalPrice = cart.products.reduce(
+        (acc, item) => acc + item.price * item.quantity,
+        0
+      );
+      await cart.save();
+      return res.stauts(200).json(cart);
+    } else {
+      return res.status(404).json({ message: "Product not found in cart "});
+    }
+  } catch(error){
+    console.error(error);
+    return res.status(500).json({ message: "Server error"});
+  }
+});
+
+
+ 
 module.exports = router;
