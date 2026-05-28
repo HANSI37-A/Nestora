94a12069947925772ff3ff5f052d573d698014b1
HANSI37-A <tharaki115h@gmail.com>
2026-05-28 18:55:42 +0530

    Add admin product slice for managing admin product operations in Redux

---
+++ client/src/redux/slice/adminProductSlice.js
@@ -0,0 +1,111 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const API_URL = `${import.meta.env.VITE_BACKEND_URL}`;
+const USER_TOKEN = `Bearer ${localStorage.getItem("userToken")}`;
+
+//async thunk to fetch admin products
+export const fetchAdminProducts = createAsyncThunk(
+  "adminProducts/fetchProducts",
+  async () => {
+    const response = await axios.get(`${API_URL}/api/admin/products`, {
+      headers: {
+        Authorization: USER_TOKEN,
+      },
+    });
+    return response.data;
+  }
+);
+
+// Async function to create a new product 
+export const createProduct = createAsyncThunk(
+  "adminProducts/createProduct",
+  async (prodcutData) => {
+    const response = await axios.post(
+      `${API_URL}/api/admin/products`,
+      prodcutData,
+      {
+        headers: {
+          Authorization: USER_TOKEN,
+        },
+      }
+    );
+    return response.data;
+  }
+);
+
+// async thunk to uspade an existing product
+export const updateProduct = createAsyncThunk(
+  "adminProducts/updateProduct",
+  async ({ id, prodcutData }) => {
+    const response =await axios.put(
+      `${API_URL}/api/admin/products/${id}`,
+      prodcutData,
+      {
+        headers: {
+          Authorization: USER_TOKEN,
+        },
+      }
+    );
+    return response.data;
+  }
+);
+
+// asunc thunk to delete a product
+export const deleteProduct = createAsyncThunk(
+  "adminProducts/deleteProduct",
+  async (id) => {
+    await axios.delete(`${API_URL}/api/admin/products/${id}`,{
+      headers: {Authorization: USER_TOKEN},
+    });
+    return id;
+  }
+);
+
+const adminProductSlice = creatSlice({
+  name: "adminProducts",
+  initialState: {
+    products: [],
+    loading: false,
+    error: null,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+    .addCase(fetchAdminProducts.pending, (state) => {
+      state.loading = true;
+    })
+    .addCase(fetchAdminProducts.fulfilled, (state, action) => {
+      state.loading = false;
+      state.products = action.payload;
+    })
+    .addCase(fetchAdminProducts.rejected, (state, action) => {
+      state.loading = false;
+      state.error = action.error.message;
+    })
+
+    // create product
+    .addCase(createProduct.fulfilled, (state, action) => {
+      state.products.push(action.payload);
+    })
+
+    // update product
+    .addCase(updateProduct.fulfilled, (state, action) => {
+      const index = state.products.findIndex(
+        (product) => product._id === action.payload._id
+      );
+      if(index !== -1){
+        state.products[index] = action.payload;
+      }
+    })
+    
+    // delete product
+    .addCase(deleteProduct.fulfilled, (state, action) => {
+      state.products = state.products.filter(
+        (product) => product._id !== action.payload
+      );
+    });
+  },
+});
+
+export default adminProductSlice.reducers;
